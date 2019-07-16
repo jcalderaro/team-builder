0aaf8250a230e2882f62a6579c900cb18d031635
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-07-16 11:15:33 -0700

    Final Solution Zoom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,10 @@
-import React, {useState} from "react"
-// import React, {useState} from "react"
-
-import './App.css';
-// import './App.css';
+import React, { useState } from 'react';
 
 import { team } from './team';
-// import TeamList from "./components/TeamList";
-
 import Form from './components/Form.js';
-// import AddForm from './components/AddForm.js';
+import MemberList from './components/MemberList.js';
 
-import MemberList from './components/Memberlist.js'
-// I did not have a page for this
-
-
-
-// // // // // // // // // // // // // // // // // // // // 
+import './App.css';
 
 function App() {
 
@@ -56,63 +45,3 @@ function App() {
 }
 
 export default App;
-
-// // // // // // // // // // // // // // // // // // // // 
-
-
-/* 
-function App() {
-  const [teamList, setTeamList] = useState([{
-    name: "Pikachu",
-    email: "Pikachu@gmail.com", 
-    role: "Electric",
-    card: "1"
-  },
-  {
-    name: "Bulbasaur",
-    email: "Bulbasaur@gmail.com", 
-    role: "Grass",
-    card: "2"
-  },
-  {
-    name: "Charmander",
-    email: "Charmander@gmail.com", 
-    role: "Fire",
-    card: "3"
-  },
-  {
-    name: "Ekans",
-    email: "Snake@gmail.com", 
-    role: "Poison",
-    card: "4"
-  },
-  {
-    name: "Sandshrew",
-    email: "Sandshrew@gmail.com", 
-    role: "Ground",
-    card: "5"
-  },
-  {
-    name: "Clefairy",
-    email: "Clefairy@gmail.com", 
-    role: "Fairy",
-    card: "6"
-  }])
-
-
-
-
-  console.log (teamList) 
-
-  return (
-    <div className="App">
-     <h1> Pokemon Team Builder </h1>
-     <AddForm setTeamList={setTeamList} teamList={teamList} />
-     <TeamList teamList = {teamList} />
-     
-    </div>
-  );
-}
-
-export default App;
-*/
